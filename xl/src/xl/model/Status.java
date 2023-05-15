84ba2b6ba3fd53da0709c2c5da9d7196a47547a8
zikav1 <hugo@rolfs.me>
2023-05-15 13:46:28 +0200

    Status class done

--- xl/src/xl/model/Status.java
+++ xl/src/xl/model/Status.java
@@ -2,8 +2,21 @@ package xl.model;
 
 public class Status {
     
+    private String status;
 
-    public Status(){
-        
+    public Status(String status){
+        this.status = status;
+    }
+
+    public void clearStatus(){
+        status = "";
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String s){
+        status = s;
     }
 }
