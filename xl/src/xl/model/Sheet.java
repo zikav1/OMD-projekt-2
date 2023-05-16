1e05f1fe05120973e86bef826fe7e9e693783843
zikav1 <hugo@rolfs.me>
2023-05-16 11:44:49 +0200

    updated UML

--- xl/src/xl/model/Sheet.java
+++ xl/src/xl/model/Sheet.java
@@ -36,7 +36,6 @@ public class Sheet extends Observable implements Environment {
             sheetMap.put(slotName, new CommentSlot(value));
             setChanged();
             notifyObservers();
-            printSheet();
             return true;
         }
 
@@ -50,7 +49,6 @@ public class Sheet extends Observable implements Environment {
             status.setStatus("Input does not deliver data");
             setChanged();
             notifyObservers();
-            printSheet();
             return false;
         }
         catch(XLException e2){
@@ -58,7 +56,6 @@ public class Sheet extends Observable implements Environment {
             status.setStatus("Invalid expression");
             setChanged();
             notifyObservers();
-            printSheet();
             return false;
         }
 
@@ -71,7 +68,6 @@ public class Sheet extends Observable implements Environment {
             status.setStatus("Blank slots resulting in null value");
             setChanged();
             notifyObservers();
-            printSheet();
             return false;
         }
         
@@ -81,7 +77,6 @@ public class Sheet extends Observable implements Environment {
             status.setStatus("Circular reference detected");
             setChanged();
             notifyObservers();
-            printSheet();
             return false;
         }
 
@@ -89,7 +84,6 @@ public class Sheet extends Observable implements Environment {
         sheetMap.put(slotName, new ExprSlot(expr));
         setChanged();
         notifyObservers();
-        printSheet();
         return true;
 
     }
@@ -155,24 +149,5 @@ public class Sheet extends Observable implements Environment {
 
     public String getStatus(){
         return status.getStatus();
-    }
-
-    public void printSheet(){
-        // String[] addresses = {
-        //     "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10",
-        //     "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10",
-        //     "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10",
-        //     "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10",
-        //     "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10",
-        //     "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10",
-        //     "G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "G10",
-        //     "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10",
-        // };
-
-        // for(String address : addresses){
-        //     if(sheetMap.containsKey(address)) System.out.println(address + ": " + sheetMap.get(address).getSlotValue(this));
-        //     else System.out.println(address + ": blank" );
-        // }
-        System.out.println("Print sheet");
     }
 }
